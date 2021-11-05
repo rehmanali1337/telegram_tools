02c4ce5e45fa3d3ee5b1826af46bec2dd3d5cdba
Rehman Ali <rehmanali.9442289@gmail.com>
2021-11-05 11:33:53 +0500

    Upgrade

--- telegram_tools/client.py
+++ telegram_tools/client.py
@@ -1,11 +1,10 @@
 from telethon import TelegramClient, errors, types
-import traceback
-import asyncio
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from telethon.tl import functions
 from telethon.tl.functions.channels import JoinChannelRequest
 import os
 import pickle
+from cache_it import PersistantDict
 
 
 class Client(TelegramClient):
@@ -14,16 +13,18 @@ class Client(TelegramClient):
         _dir = "do_not_delete"
         os.makedirs(_dir, exist_ok=True)
         self._joins_list_file = f"{_dir}/{self.session.filename.split('/')[-1]}_joinslist"
+        self._joins_cache = PersistantDict(
+            f'{self.session.filename.split("/")[-1]}_joins')
 
     @staticmethod
-    def standardize_id(id: str) -> str:
+    def standardize_id(id: str) -> int:
         id = str(id)
-        current_id = id
+        current_id = str(id)
         if str(id[0]) == '-':
             current_id = str(id[1:])
             if current_id[0:3] == '100':
                 current_id = current_id[3:]
-        return current_id
+        return int(current_id)
 
     def _dump_to_file(self, file, obj: object):
         with open(file, "wb") as f:
@@ -45,12 +46,11 @@ class Client(TelegramClient):
             joins_list.extend(self._read_from_file(self._joins_list_file))
         self._dump_to_file(self._joins_list_file, joins_list)
 
-    async def join_it(self, target_link: str, ignore_cache=False):
+    async def join_it(self, target_link: str, ignore_cache=False) -> int:
         if not ignore_cache:
-            if self._have_joined_already(target_link):
-                print(f"User have already joined {target_link}")
-                self._add_to_joinslist(target_link)
-                return await self.get_input_entity(target_link)
+            if target_link in self._joins_cache:
+                print(f'User have already joined {target_link} ')
+                return await self._joins_cache.get(target_link)
         try:
             # Join public group
             updates = await self(JoinChannelRequest(target_link))
@@ -60,11 +60,13 @@ class Client(TelegramClient):
             try:
                 updates = await self(ImportChatInviteRequest(group_hash))
             except errors.rpcerrorlist.UserAlreadyParticipantError:
-                self._add_to_joinslist(target_link)
-                return await self.get_input_entity(target_link)
+                return await self._joins_cache.get(target_link)
         if isinstance(updates, list):
-            updates = updates[0]
-        self._add_to_joinslist(target_link)
+            updates = updates[0].id
+        elif isinstance(updates, types.Updates):
+            updates = updates.chats[0].id
+
+        await self._joins_cache.add(target_link, updates)
         return updates
 
     async def delete_all_schedule_messages(self, target_group: types.Dialog):
